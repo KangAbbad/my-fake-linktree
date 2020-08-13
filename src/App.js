45e5541b8b81869adcbf459a0f804b2b08aa0257
kangabbad <naufalabbad9@gmail.com>
2020-08-13 11:15:00 +0700

    Template

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import AvatarWebp from './images/avatar.webp';
+import AvatarPng from './images/avatar.png';
+
+const dataLink = [
+  {
+    label: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/kang-abbad/'
+  },
+  {
+    label: 'Personal Site',
+    url: 'https://kangabbad.netlify.app'
+  },
+  {
+    label: 'Telegram',
+    url: 'https://t.me/kangabbad'
+  },
+  {
+    label: 'WhatsApp',
+    url: "https://api.whatsapp.com/send?phone=87739999776&text=Samlekom%20mhamanx..."
+  },
+]
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className='container'>
+      <div className='content'>
+        <ImgWithFallback
+          src={AvatarWebp}
+          fallback={AvatarPng}
+          alt="My Avatar"
+          className='avatar'
+          height='120'
+          width='120'
+        />
+        <p className='username'>
+          @kangabbad
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p className='username'>
+          Muhammad Naufal Abbad
+        </p>
+        <p className='bio'>
+          Tamvan dan Berani | Javascript Enthusiast | Frontend Developer
+        </p>
+        <ul className='data-list'>
+          {dataLink.map((item, index) => (
+            <li className='data-list--item'>
+              <button
+                key={index}
+                className='button-link'
+                onClick={() => {
+                  setTimeout(() => {
+                    window.open(item.url)
+                  }, 500)
+                }}
+              >
+                {item.label}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+const ImgWithFallback = ({
+  src,
+  fallback,
+  type = 'image/webp',
+  ...delegated
+}) => {
+  return (
+    <picture>
+      <source srcSet={src} type={type} />
+      <img src={fallback} alt={delegated.alt} {...delegated} />
+    </picture>
+  );
+};
