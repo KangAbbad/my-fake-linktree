7714885d39adce2d1e15b9a3118d75d46342bf0b
kangabbad <naufalabbad9@gmail.com>
2020-08-23 00:49:38 +0700

    feature/basic-02: Refactor code

--- src/App.js
+++ src/App.js
@@ -28,16 +28,7 @@ const dataLink = [
 const App = () => {
   return (
     <div className='container'>
-      <div
-        style={{
-          position: 'absolute',
-          top: 0,
-          right: 0,
-          bottom: 0,
-          left: 0,
-          zIndex: -1
-        }}
-      >
+      <div className='background'>
         <Img
           style={{
             backgroundColor: '#1C2329',
